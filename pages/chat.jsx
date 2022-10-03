1a8b92ac2b5130aaf6864f692c239789fcf97843
Min Htet Ko <penguincrab99@gmail.com>
2022-10-03 16:03:03 +0630

    Added turn servers to connect through NAT

--- pages/chat.jsx
+++ pages/chat.jsx
@@ -50,7 +50,24 @@ export default function Chat() {
 
     function createPeerConnection(signaling) {
         const peerConnection = new RTCPeerConnection({
-            iceServers: [{ urls: "stun:openrelay.metered.ca:80" }],
+            iceServers: [
+                { urls: "stun:openrelay.metered.ca:80" },
+                {
+                    urls: "turn:openrelay.metered.ca:80",
+                    username: "openrelayproject",
+                    credential: "openrelayproject",
+                },
+                {
+                    urls: "turn:openrelay.metered.ca:443",
+                    username: "openrelayproject",
+                    credential: "openrelayproject",
+                },
+                {
+                    urls: "turn:openrelay.metered.ca:443?transport=tcp",
+                    username: "openrelayproject",
+                    credential: "openrelayproject",
+                },
+            ],
         });
 
         peerConnection.onnegotiationneeded = async () => {
